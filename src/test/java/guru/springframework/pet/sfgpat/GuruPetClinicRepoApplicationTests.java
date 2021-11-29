1497d56465361eed966c037e8f03d6cbe59036d6
miguel <m.gutierrez@peak-14.de>
2021-11-29 11:59:56 +0100

    Initial commit

---
+++ src/test/java/guru/springframework/pet/sfgpat/GuruPetClinicRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.pet.sfgpat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruPetClinicRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

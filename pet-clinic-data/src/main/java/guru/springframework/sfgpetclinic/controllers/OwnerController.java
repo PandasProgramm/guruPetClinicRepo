395314a4c71a54ae68eb727c28829d97f6e70452
miguel <m.gutierrez@peak-14.de>
2021-12-01 09:15:00 +0100

    -fix Beans on Controllers: - OwnerController - PetController

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,9 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.models.Owner;
 import guru.springframework.sfgpetclinic.services.CrudService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public record OwnerController(@Qualifier("ownerServiceMap") CrudService<Owner, Long> ownerServiceMap) {
 
     @GetMapping("/owners")
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
@@ -3,9 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.models.Pet;
 import guru.springframework.sfgpetclinic.services.CrudService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public record PetController(@Qualifier("petServiceMap") CrudService<Pet, Long> petServiceMap) {
 
 

e1e0d8b85cc471c4029d17381098387d5db26374
miguel <m.gutierrez@peak-14.de>
2021-12-02 08:48:36 +0100

    -adding spring config closes #17

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,18 +4,13 @@ import guru.springframework.sfgpetclinic.models.Owner;
 import guru.springframework.sfgpetclinic.models.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public record DataLoader(OwnerService ownerService, VetService vetService) implements CommandLineRunner {
 
-    public DataLoader {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
-    }
 
     @Override
     public void run(String... args) throws Exception {
